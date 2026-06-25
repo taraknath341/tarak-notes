13225d3f837d5ba0c1c868d63625bd9f4d0318a9
Taraknath Karan <taraknath341@gmail.com>
2026-06-25 21:29:12 +0530

    title.trim() of entire backend

--- schema/note.schema.js
+++ schema/note.schema.js
@@ -2,23 +2,26 @@ import { createNoteValidator, deleteNoteValidator, updateNoteValidator } from ".
 
 class createNoteSchema {
    constructor({ title, content }) {
+      title = title.trim();
       createNoteValidator({ title, content });
-      this.title = title.trim();
+      this.title = title;
       this.content = content;
    }
 }
 
 class deleteNoteSchema {
    constructor({ title }) {
+      title = title.trim();
       deleteNoteValidator({ title });
-      this.title = title.trim();
+      this.title = title;
    }
 }
 
 class updateNoteSchema {
    constructor({ title, content }) {
+      title = title.trim();
       updateNoteValidator({ title, content });
-      this.title = title.trim();
+      this.title = title;
       this.content = content;
    }
 }
